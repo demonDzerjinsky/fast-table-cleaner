1141be66ff651a4279e4cb483ee36707c858f963
vtb4043344 <dcherkunov@vtb.ru>
2025-10-03 23:35:34 +0700

    add: logback, connection db test

---
+++ src/test/java/ru/ssp/api/ApiIntegrationTest.java
@@ -0,0 +1,41 @@
+package ru.ssp.api;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Testcontainers
+public class ApiIntegrationTest {
+
+    @Container
+    private static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15.3")
+            .withDatabaseName("testdb")
+            .withUsername("user")
+            .withPassword("pass");
+
+    @Test
+    void checkContainer() {
+        var url = postgres.getJdbcUrl();
+        var user = postgres.getUsername();
+        var pass = postgres.getPassword();
+        log.info("container url = {}", url);
+        Assertions.assertAll(
+                () -> assertEquals("user", user),
+                () -> assertEquals("pass", pass),
+                () -> assertNotNull(url));
+    }
+
+    @Test
+    void main() {
+        assertTrue(true);
+    }
+}
