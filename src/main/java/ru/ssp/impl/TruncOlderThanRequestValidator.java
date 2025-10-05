4e4fba71071bb099bd5da00d08951bfbb4548f03
vtb4043344 <dcherkunov@vtb.ru>
2025-10-06 00:51:22 +0700

    add: validator base design

---
+++ src/main/java/ru/ssp/impl/TruncOlderThanRequestValidator.java
@@ -0,0 +1,31 @@
+package ru.ssp.impl;
+
+import static ru.ssp.dto.TruncOlderThanResponse.Status.FAIL;
+
+import java.util.Optional;
+
+import ru.ssp.dto.TruncOlderThanRequest;
+import ru.ssp.dto.TruncOlderThanResponse;
+
+/**
+ * валидирует входной контракт вызова api TruncOlderThanRequest.
+ */
+class TruncOlderThanRequestValidator
+        implements Validator<TruncOlderThanRequest, TruncOlderThanResponse> {
+    /**
+     * сообщение в сл ошибки валидации.
+     */
+    private static final String CONTR_VALID_ERR = "";
+
+    @Override
+    public Optional<TruncOlderThanResponse> validate(
+            final TruncOlderThanRequest req) {
+        return Optional.of(req).filter(this::checkIfErr)
+                .map(p -> new TruncOlderThanResponse(FAIL, CONTR_VALID_ERR));
+    }
+
+    private boolean checkIfErr(
+            final TruncOlderThanRequest req) {
+        return true; // todo
+    }
+}
