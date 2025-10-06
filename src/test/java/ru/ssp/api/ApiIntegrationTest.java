b96a8ada5b09abc8533ac171c43051426ea90446
vtb4043344 <dcherkunov@vtb.ru>
2025-10-06 19:47:23 +0700

    add: connection pool in tests

--- src/test/java/ru/ssp/api/ApiIntegrationTest.java
+++ src/test/java/ru/ssp/api/ApiIntegrationTest.java
@@ -1,17 +1,26 @@
 package ru.ssp.api;
 
+import static java.lang.Integer.valueOf;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static ru.ssp.infra.CustomProperties.getPty;
 
+import java.sql.SQLException;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import lombok.extern.slf4j.Slf4j;
+import ru.ssp.infra.CustomConnectionPool;
 
 @Slf4j
 @Testcontainers
@@ -19,6 +28,8 @@ public class ApiIntegrationTest {
 
     private final static String login = getPty("db.username");
     private final static String pass = getPty("db.password");
+    private final static Integer pool = valueOf(getPty("db.poolsize"));
+    private static HikariDataSource dataSource;
 
     @Container
     private static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15.3")
@@ -26,10 +37,28 @@ public class ApiIntegrationTest {
             .withUsername(login)
             .withPassword(pass);
 
+    @BeforeAll
+    static void beforeAll() {
+        final var config = new HikariConfig();
+        config.setJdbcUrl(postgres.getJdbcUrl());
+        config.setUsername(login);
+        config.setPassword(pass);
+        config.setMaximumPoolSize(pool);
+        dataSource = new HikariDataSource(config);
+    }
+
     @Test
     void checkConfigUserAndPassIsNotNull() {
         assertEquals("testuser", login);
         assertEquals("testpass", pass);
+    }
+
+    @Test
+    void checkConnection() throws SQLException {
+        assertNotNull(dataSource);
+        try (var connection = dataSource.getConnection()) {
+            assertNotNull(connection);
+        }
     }
 
     @Test
@@ -47,5 +76,13 @@ public class ApiIntegrationTest {
     @Test
     void main() {
         assertTrue(true);
+    }
+
+    @AfterAll
+    static void afterAll() {
+        if (dataSource != null && !dataSource.isClosed()) {
+            log.info("closing connection pool");
+            dataSource.close();
+        }
     }
 }
